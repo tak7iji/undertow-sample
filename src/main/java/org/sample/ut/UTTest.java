6eae677d047a4c4697b6e244002644f5583ae4fc
Eiji Takahashi <tak7iji@gmail.com>
2013-12-25 14:47:41 +0900

    refactoring

--- src/main/java/org/sample/ut/UTTest.java
+++ src/main/java/org/sample/ut/UTTest.java
@@ -5,7 +5,9 @@ import io.undertow.servlet.api.DeploymentInfo;
 import io.undertow.servlet.api.DeploymentManager;
 import io.undertow.servlet.api.ServletContainer;
 import io.undertow.servlet.api.ServletInfo;
+import io.undertow.servlet.core.ManagedServlet;
 import io.undertow.servlet.handlers.ServletHandler;
+import io.undertow.servlet.spec.ServletContextImpl;
 
 import javax.servlet.ServletException;
 
@@ -13,23 +15,19 @@ import javax.servlet.ServletException;
  * Created by mash on 13/12/25.
  */
 public class UTTest {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ServletException {
         final ServletContainer container = ServletContainer.Factory.newInstance();
         DeploymentInfo servletBuilder = new DeploymentInfo()
                 .setClassLoader(UTTest.class.getClassLoader())
                 .setContextPath("/myapp")
-                .setDeploymentName("test.war")
+                .setDeploymentName("myapp.war")
                 .addServlets(
                         new ServletInfo("MyServlet", SimpleServlet.class)
-                                .addMapping("/"));
+                                .addMapping("/hello"));
 
         DeploymentManager manager = container.addDeployment(servletBuilder);
         manager.deploy();
 
-        try {
-            Undertow.builder().addListener(8888, "localhost").setHandler(manager.start()).build().start();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        Undertow.builder().addListener(8888, "localhost").setHandler(manager.start()).build().start();
     }
 }
